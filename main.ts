d93937b4614588ca3b3124e64be83ceb1d3646b7
Zach Auten <zachauten@gmail.com>
2025-08-27 03:19:00 +0000

    second commit

--- main.ts
+++ main.ts
@@ -1,12 +1,34 @@
 import gitlog from "npm:gitlog";
 import { parseArgs } from "jsr:@std/cli/parse-args";
 
+type LogLevel = "debug" | "info" | "warn" | "error" | "quiet";
+
+const types = [
+  "build",
+  "chore",
+  "ci",
+  "docs",
+  "feat",
+  "fix",
+  "perf",
+  "refactor",
+  "revert",
+  "style",
+  "test",
+];
+
 async function main() {
   const flags = parseArgs(Deno.args, {
-  boolean: ["help"],
-  string: ["number"],
-  default: { number: "1" },
+    boolean: ["help", "quiet"],
+    string: ["level", "number"],
+    default: { level: "info", number: "1" },
   });
+
+  if (flags.quiet) {
+    setLogLevel("quiet");
+  } else {
+    setLogLevel(flags.level as LogLevel);
+  }
 
   const number = parseInt(flags.number);
 
@@ -14,14 +36,96 @@ async function main() {
     const commits = await gitlog({
       repo: "./",
       number,
-      fields: ["subject", "body", "hash"]
-    })
-    console.log(commits);
-  } catch(error) {
-    if(/fatal: your current branch '\w+' does not have any commits yet/.test((error as Error).message)) {
-      console.error("Error: repository does not have any commits");
+      fields: ["subject", "body", "hash"],
+    });
+    console.debug("%cDebug: found commits ", "color:blue", commits);
+    for (const commit of commits) {
+      console.debug("%cDebug: parsing commit " + commit.hash, "color:blue");
+      parseSubject(commit.subject);
+    }
+  } catch (error) {
+    if (
+      /fatal: your current branch '\w+' does not have any commits yet/.test(
+        (error as Error).message,
+      )
+    ) {
+      console.error(
+        "%cError: repository does not have any commits",
+        "color:red",
+      );
     }
   }
+}
+
+function setLogLevel(level: LogLevel) {
+  switch (level) {
+    /* falls through */
+    case "quiet":
+      console.error = () => {};
+    /* falls through */
+    case "error":
+      console.warn = () => {};
+    /* falls through */
+    case "warn":
+      console.info = () => {};
+      console.log = () => {};
+    /* falls through */
+    case "info":
+      console.debug = () => {};
+  }
+}
+
+export function parseSubject(subject: string): number {
+  for (const type of types) {
+    if (subject.startsWith(type)) {
+      console.debug("%cDebug: found type " + type, "color:blue");
+      const rest = subject.substring(subject.indexOf(type) + type.length);
+      console.debug(`%cDebug: parsing "${rest}"`, "color:blue");
+      return parseScope(rest);
+    }
+  }
+  console.error(`%cError: subject missing type "${subject}"`, "color:red");
+  return 1;
+}
+
+function parseScope(subject: string): number {
+  console.debug("%cDebug: parsing scope from " + subject, "color:blue");
+  const match = subject.match(/^(\(\w+\))(.+)$/);
+  if (!match) {
+    console.debug("%cDebug: no scope found", "color:blue");
+    return parseExclaimation(subject);
+  } else {
+    console.debug("%cDebug: found scope " + match[1], "color:blue");
+    return parseExclaimation(match[2]);
+  }
+}
+
+function parseExclaimation(subject: string): number {
+  console.debug("%cDebug: parsing '!' from " + subject, "color:blue");
+  if (subject.startsWith("!")) {
+    console.debug("%cDebug: found '!'", "color:blue");
+    return parseColonAndSpace(subject.substring(1));
+  } else {
+    console.debug("%cDebug: no '!' found", "color:blue");
+    return parseColonAndSpace(subject);
+  }
+}
+
+function parseColonAndSpace(subject: string): number {
+  console.debug("%cDebug: parsing ':' from " + subject, "color:blue");
+  if (subject.startsWith(": ")) {
+    console.debug("%cDebug: found ': '", "color:blue");
+    return 0;
+  } else {
+    console.error("%cError: no ': ' found", "color:red");
+    return 1;
+  }
+}
+
+function _parseBody() {
+}
+
+function _parseFooters() {
 }
 
 main();
--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,51 @@
 import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
+import { parseSubject } from "./main.ts";
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+// TODO: Add tests for each type
+
+Deno.test("feat(scope)!: a type, scope, !, :, and description", () => {
+  const res = parseSubject(
+    "feat(scope)!: a type, scope, !, :, and description",
+  );
+  assertEquals(res, 0);
+});
+
+Deno.test("feat!: a type, !, :, and description", () => {
+  const res = parseSubject("feat!: a type, !, :, and description");
+  assertEquals(res, 0);
+});
+
+Deno.test("feat: a subject description", () => {
+  const res = parseSubject("feat: a subject description");
+  assertEquals(res, 0);
+});
+
+Deno.test("feat(scope): a type, scope, :, and description", () => {
+  const res = parseSubject("feat(scope): a type, scope, :, and description");
+  assertEquals(res, 0);
+});
+
+Deno.test("empty string", () => {
+  const res = parseSubject("");
+  assertEquals(res, 1);
+});
+
+Deno.test(": no type", () => {
+  const res = parseSubject(": no type");
+  assertEquals(res, 1);
+});
+
+Deno.test("no semicolon", () => {
+  const res = parseSubject("no semicolon");
+  assertEquals(res, 1);
+});
+
+Deno.test("invalid feature: ", () => {
+  const res = parseSubject("invalid feature: ");
+  assertEquals(res, 1);
+});
+
+Deno.test("feat:missing space", () => {
+  const res = parseSubject("feat:missing space");
+  assertEquals(res, 1);
 });
